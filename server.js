e1dd305f7cacdd5f74b0a072f4fa6be975b174a3
Daniela <daanituty@gmail.com>
2024-06-11 00:38:25 -0400

    correccion release y end

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const chalk = require("chalk");
 const { Pool } = require("pg");
 
-//CONFIG BD
+// CONFIG BD
 const config = {
   user: "postgres",
   host: "localhost",
@@ -10,53 +10,47 @@ const config = {
   port: 5432,
 };
 
-let pool;
+const pool = new Pool(config);
 
-const connectDB = async () => {
-  try {
-    pool = new Pool(config);
-    console.log(chalk.green.bold("Conexión exitosa a la BD"));
-  } catch (e) {
-    console.error(chalk.red.bold("Error al conectar a la BD:", e.message));
-    process.exit(1); 
-  }
-};
-
-//VALIDACION ARGS
+// VALIDACION ARGS
 const validateArgs = (args, lengthEsperado) => {
   if (args.length !== lengthEsperado) {
     throw new Error(`Número incorrecto de argumentos. Se esperaban ${lengthEsperado} y se recibieron ${args.length}`);
   }
 };
 
-//AGREGAR
+// AGREGAR
 const nuevo = async (args) => {
   validateArgs(args, 4);
   const [rut, nombre, curso, nivel] = args;
+  const client = await pool.connect();
 
   try {
     const consulta = {
       text: "INSERT INTO estudiante (rut, nombre, curso, nivel) VALUES ($1, $2, $3, $4)",
       values: [rut, nombre, curso, nivel],
     };
-    await pool.query(consulta);
+    await client.query(consulta);
     console.log(chalk.white.bgGreen.bold(`Estudiante ${nombre} agregado con éxito`));
   } catch (e) {
     console.error(chalk.white.bgRed.bold(`Hubo un error al ingresar datos: ${e.message}`));
+  } finally {
+    client.release();
   }
 };
 
-//EDITAR
+// EDITAR
 const editar = async (args) => {
   validateArgs(args, 4);
   const [rut, nombre, curso, nivel] = args;
+  const client = await pool.connect();
 
   try {
     const consulta = {
       text: "UPDATE estudiante SET nombre = $1, curso = $2, nivel = $3 WHERE rut = $4",
       values: [nombre, curso, nivel, rut],
     };
-    const result = await pool.query(consulta);
+    const result = await client.query(consulta);
     if (result.rowCount === 0) {
       console.log(chalk.white.bgBlueBright.bold(`No hay estudiante con RUT ${rut}`));
     } else {
@@ -64,13 +58,16 @@ const editar = async (args) => {
     }
   } catch (e) {
     console.error(chalk.white.bgRed.bold(`Hubo un error al editar datos del estudiante: ${e.message}`));
+  } finally {
+    client.release();
   }
 };
 
-//BUSCAR POR RUT
+// BUSCAR POR RUT
 const buscarPorRut = async (args) => {
   validateArgs(args, 1);
   const [rut] = args;
+  const client = await pool.connect();
 
   try {
     const consulta = {
@@ -78,7 +75,7 @@ const buscarPorRut = async (args) => {
       values: [rut],
       rowMode: "array", // ARRAY FORMAT
     };
-    const result = await pool.query(consulta);
+    const result = await client.query(consulta);
     if (result.rows.length === 0) {
       console.log(chalk.white.bgBlueBright.bold(`No se encontró estudiante con rut: ${rut}.`));
     } else {
@@ -87,13 +84,17 @@ const buscarPorRut = async (args) => {
     }
   } catch (e) {
     console.error(chalk.white.bgRed.bold(`Hubo un error al buscar al estudiante: ${e.message}`));
+  } finally {
+    client.release();
   }
 };
 
-//LISTAR - MOSTRAR TODOS
+// LISTAR - MOSTRAR TODOS
 const listarEstudiantes = async () => {
+  const client = await pool.connect();
+
   try {
-    const result = await pool.query({
+    const result = await client.query({
       text: "SELECT rut, nombre, curso, nivel FROM estudiante",
       rowMode: "array",
     });
@@ -105,20 +106,23 @@ const listarEstudiantes = async () => {
     }
   } catch (e) {
     console.error(chalk.white.bgRed.bold(`Hubo un error al buscar: ${e.message}`));
+  } finally {
+    client.release();
   }
 };
 
-//ELIMINAR
+// ELIMINAR
 const eliminar = async (args) => {
   validateArgs(args, 1);
   const [rut] = args;
+  const client = await pool.connect();
 
   try {
     const consulta = {
       text: "DELETE FROM estudiante WHERE rut = $1",
       values: [rut],
     };
-    const result = await pool.query(consulta);
+    const result = await client.query(consulta);
     if (result.rowCount === 0) {
       console.log(chalk.white.bgBlueBright.bold(`No hay estudiante con RUT ${rut}`));
     } else {
@@ -126,13 +130,13 @@ const eliminar = async (args) => {
     }
   } catch (e) {
     console.error(chalk.white.bgRed.bold(`Hubo un error al eliminar: ${e.message}`));
+  } finally {
+    client.release();
   }
 };
 
-//MAIN
+// MAIN
 const main = async () => {
-  await connectDB(); 
-
   const args = process.argv.slice(2);
   const option = args[0]; // OPCION
   const argsFunction = args.slice(1); // ARGS
